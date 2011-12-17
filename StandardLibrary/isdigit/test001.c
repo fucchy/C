094481fab2d313a241d1ba36682d27a75ca3cdeb
john <john@john-desktop.(none)>
2011-12-17 17:54:03 +0900

    add isdigit/

---
+++ StandardLibrary/isdigit/test001.c
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+int main(int argc,char *argv[]){
+	//int a;
+	//a=getchar();
+	//if(isdigit(a)!=0){
+	//	printf("あなたが入力した数字は%dです。\n",(char)(a));
+	//}
+	printf("%d\n",isdigit('a'));
+	printf("%d\n",3);
+	return EXIT_SUCCESS;
+}
+
