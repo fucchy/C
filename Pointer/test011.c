f04dc5064e2e5522b5f722b769a7bfe6ee8339e5
john <john@john-desktop.(none)>
2011-12-20 10:21:49 +0900

    add test011.c and test012.c

---
+++ Pointer/test011.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc,char *argv[]){
+	char *s = "ABC";
+	printf(s);
+
+    //char s='A';
+	//printf(s);
+	return EXIT_SUCCESS;
+}
+
---
+++ Pointer/test012.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[]){
+    char s[100];
+
+	scanf("%s",s);
+	printf("%s",s);
+
+	return 0;
+}
+
