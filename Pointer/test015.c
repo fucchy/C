a785d0980efbd812f48d70c8f04c5e6db51a9a4b
john <john@john-desktop.(none)>
2011-12-21 10:51:12 +0900

    add test015.c

--- Pointer/test015.c
+++ Pointer/test015.c
@@ -8,19 +8,19 @@
 
 int main(int argc,char *argv[]){
     int array[]={0,1,2,3,4,1,15,11,16,34,6,444}; // 配列arrayの宣言と初期化
-	int arrayNum=sizeof(array)/sizeof(array[0]); // 配列arrayの要素数をarrayNumに代入
-	int i;
+    int arrayNum=sizeof(array)/sizeof(array[0]); // 配列arrayの要素数をarrayNumに代入
+    int i;
     int max=array[0]; // とりあえず、array[0]の値を最大値を入れるmaxオブジェクトに代入しておく
 
     // array[1]から始めて、maxの値より大きければ、maxの値を上書きする
-	for(i=1;i<arrayNum;i++){
-	    if(max<array[i]){
-		    max=array[i];
-	    }
+    for(i=1;i<arrayNum;i++){
+        if(max<array[i]){
+            max=array[i];
+        }
     }
 
-	printf("%d\n",max);
+    printf("%d\n",max);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
