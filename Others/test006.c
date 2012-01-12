d5a755bef8b7b2924c6de1aec1a6991ce640c12e
john <john@john-desktop.(none)>
2012-01-12 12:12:08 +0900

    add test006.c

---
+++ Others/test006.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void out(char *p);
+
+int main(int argc,char *argv[]){
+	int b[10]={0};
+  char *a="test";
+	out(a);
+	printf("%p\n",(void*)b);
+	printf("%d\n",b[0]);
+	printf("%d\n",b[0]);
+	printf("%d\n",b[1]);
+	printf("%d\n",sizeof(b)/sizeof(b[0]));
+	return EXIT_SUCCESS;
+}
+
+void out(char *p){
+    printf("%s\n",p);
+}
+
