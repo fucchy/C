d84c1c10462a3aa0e546c7f7e0757882e03c344f
john <john@john-desktop.(none)>
2012-01-12 12:13:24 +0900

    rewrite test006.c

--- Others/test006.c
+++ Others/test006.c
@@ -5,15 +5,15 @@
 void out(char *p);
 
 int main(int argc,char *argv[]){
-	int b[10]={0};
+  int b[10]={0};
   char *a="test";
-	out(a);
-	printf("%p\n",(void*)b);
-	printf("%d\n",b[0]);
-	printf("%d\n",b[0]);
-	printf("%d\n",b[1]);
-	printf("%d\n",sizeof(b)/sizeof(b[0]));
-	return EXIT_SUCCESS;
+  out(a);
+  printf("%p\n",(void*)b);
+  printf("%d\n",b[0]);
+  printf("%d\n",b[0]);
+  printf("%d\n",b[1]);
+  printf("%d\n",sizeof(b)/sizeof(b[0]));
+  return EXIT_SUCCESS;
 }
 
 void out(char *p){
