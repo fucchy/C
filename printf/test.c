29b56eca101d4d25e161d121522d6325bdc1071c
John Connor <john@localhost.localdomain>
2016-01-19 17:06:29 +0900

    trigraphとprintfの返却値について追記

--- printf/test.c
+++ printf/test.c
@@ -1,3 +1,7 @@
+// C99に準拠
+//   ISO/IEC 9899:1999とISO/IEC 9899:1999 TC1
+//   (JIS X 3010:2003)
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,6 +12,23 @@ int main(void)
   // 数字を表示
   printf("%d\n",10); 
 
+  // 数66が格納されている変数aをそれぞれの書式で出力
+  // %d : 10進数で出力(a=66の場合、「66」と表示)
+  // %x : 16進数で出力(a=66の場合、「42」と表示)
+  // %c : 文字で出力(a=66の場合、その環境の文字コードの66に該当する文字を出力
+  int a = 66;
+  printf("%d %x %c\n", a, a, a);
+
+  // 5.2.1.1 3文字表記(trigraph sequence)
+  fprintf(stdout, "Eh??=\n");
+
+  // printf関数の型はint
+  // printf関数の返却値は、書き出された文字数を返す。
+  // 出力エラー又は表現形式エラーが発生した場合、負の値を返す。
+  int b = 0;
+  b = printf("hello!\n");
+  printf("%d\n",b);
+  printf("helloworld:%d\n", printf("aaa"));
 
   return EXIT_SUCCESS;
 }
